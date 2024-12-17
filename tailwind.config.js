66b11bd53d428d3b21ee4ace4f09dd9b3165b3e6
punkestu <pangestubima89@gmail.com>
2024-12-17 12:38:53 +0700

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import flowbite from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,5 +10,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [flowbite],
 };
